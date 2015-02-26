d713af7f8d40932fdc3a691387aba76535f44b4b
Jan Gabriel <jan.gabriel@tul.cz>
2015-02-26 20:52:41 +0100

    pridana welcome screen na sipce v menu

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
+var gdgscl = function(){
+'use strict'
+
+var startTime = function startTime() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    if (m<10) m = "0" + m;
+    if (s<10) s = "0" + s;
+    $('.clock').each(function(){$(this).html(h+":"+m+":"+s);});
+    var t = setTimeout(function(){startTime()},500);
+}
+
 $(document).ready(function() {
-	var nextEventHref = $('#udalosti .next-event a').attr("href");
-    $('#vitejte .modal-body').load(nextEventHref.concat(' article'));
+	startTime();
+	var nextEvent = $('#udalosti .next-event a').text();
+    $('#vitejte h1').text(nextEvent);
 });
+
+};
+gdgscl();
